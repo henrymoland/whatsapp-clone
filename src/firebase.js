ba96497c2b31e85ea8802f8a0de08101fc4d1e7a
Henry Moland <henrymoland@gmail.com>
2020-09-06 14:26:26 -0700

    Created Sidebar component and Chat component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDy_eWFSGX1L_p6rNDnK4IHYM0FBt815Mo",
+  authDomain: "whatsapp-clone-811b3.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-811b3.firebaseio.com",
+  projectId: "whatsapp-clone-811b3",
+  storageBucket: "whatsapp-clone-811b3.appspot.com",
+  messagingSenderId: "158290660533",
+  appId: "1:158290660533:web:5d417f4f1f990f08e802fc",
+  measurementId: "G-2M91H3NG5K",
+};
